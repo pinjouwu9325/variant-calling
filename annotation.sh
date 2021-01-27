3b385c25b15a70a77bde967c5deae047d89cd803
Pin-Jou Wu <pinjouwu.tw@gmail.com>
2021-01-27 11:51:38 +0800

    remove annotation.sh

--- annotation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Annotate variants by VEP (version 100)
-# If set --cache, cache files should be downloaded and decompressed in ~/.vep.
-# The version of cache files should match to the version of VEP
-vep -i tumor.PASS.vcf.gz --cache --species homo_sapiens -e --fork 20 -o tumor.PASS.variant_effect.txt
